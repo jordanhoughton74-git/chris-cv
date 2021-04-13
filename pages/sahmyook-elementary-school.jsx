c57b378e235ddf94118924fdfb082f587f359889
Jordan Houghton <jordanhoughton74@gmail.com>
2021-04-13 09:22:12 +0100

    update folders, add doc.js

--- pages/sahmyook-elementary-school.jsx
+++ pages/sahmyook-elementary-school.jsx
@@ -25,7 +25,7 @@ export default function Home() {
           <p className={styles.infoText}>During my time with Sahmyook, I have been a first grade teacher, with only one class of students.  This has allowed me the opportunity to hone my teaching skills in a more intimate environment, as I only teach a 36 different students.  My class sizes range from 18 to 36 students, and I teach on my own.  This has allowed me to develop my classroom management skills, and to grow and improve as a teacher.</p>
           </Col>
           <Col className={styles.centerImg} lg={6} md={6} sm={12}>
-            <img className={styles.imageSize} src="/images/sahmyook/sahmyook-top.jpg" />
+            <img className={styles.imageSize} src="/images/Sahmyook/sahmyook-top.jpg" />
 
           </Col>
         </Row>
@@ -41,7 +41,7 @@ export default function Home() {
           <p className={styles.infoText}>  I have been teaching the students Language Arts, science and social studies, as well art.  This has allowed me to develop my skills in a wide range of areas, especially my planning and multimedia usage skills, as this the first school I have worked in that has a smartboard in the classroom.  Using these skills, I have been able to make my classes even more fun and interesting, to ensure the students are engaged in learning, giving them the best chance of language acquisition.</p>
           </Col>
           <Col className={styles.centerImg} lg={6} md={6} sm={12}>
-            <img className={styles.imageSize} src="/images/sahmyook/sahmyook-bot.png" />
+            <img className={styles.imageSize} src="/images/Sahmyook/sahmyook-bot.png" />
 
           </Col>
         </Row>
